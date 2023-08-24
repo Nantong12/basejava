20936cc4fea598ebb5b89abe104f6079d101fd30
Nantong12 <yfnfkbz1f@mail.ru>
2023-08-24 20:49:53 +0300

    Были реализованы методы в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,57 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)){
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = 0;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)){
+                storage[i] = null;
+                index = i;
+            }
+        }
+        for (int i = index; i < size() + 1; i++) {
+            storage[i] = storage[i+1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null){
+                counter ++;
+            } else{
+                break;
+            }
+        }
+        return counter;
     }
 }

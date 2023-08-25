f90e2cba4fc7309df1f7cbe84557e5e1deb1d0d6
Nantong12 <yfnfkbz1f@mail.ru>
2023-08-25 18:34:27 +0300

    Изменения в классе ArrayStorage; Добавлено поле size; Изменен порядок переноса Resume со значением null в конец массива в методе delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,20 +2,24 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
+            size = 0;
         }
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size ++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)){
                 return storage[i];
             }
@@ -24,24 +28,23 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int index = 0;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)){
-                storage[i] = null;
-                index = i;
+                storage[i] = storage[size - 1];
+                storage[size-1] = null;
+                break;
             }
         }
-        for (int i = index; i < size() + 1; i++) {
-            storage[i] = storage[i+1];
-        }
+
+        size --;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[size()];
-        for (int i = 0; i < size(); i++) {
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
         }
         return resumes;
@@ -49,14 +52,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int counter = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null){
-                counter ++;
-            } else{
-                break;
-            }
-        }
-        return counter;
+        return size;
     }
 }
